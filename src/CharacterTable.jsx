84d8b045076484f650e1982106f5ffd6080cd272
Jeremy Lucero <Lucero.jeremy07@gmail.com>
2023-07-22 03:15:22 -0500

    Fixed all issues regarding linting, rules, and prop validation

--- src/CharacterTable.jsx
+++ src/CharacterTable.jsx
@@ -1,14 +1,22 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 
-export class TableCreator extends Component {
+export class TableRow extends Component {
 	render() {
 		const prop = this.props;
 		return (
 			<tr className={prop.className}>
 				<td>{prop.name}</td>
-				<td>{prop.skillset}</td>
+				<td>{prop.skillset.join(", ")}</td>
 				<td>{prop.votes}</td>
 			</tr>
 		);
 	}
 }
+
+TableRow.propTypes = {
+	className: PropTypes.string,
+	name: PropTypes.string,
+	skillset: PropTypes.string,
+	votes: PropTypes.number,
+};
